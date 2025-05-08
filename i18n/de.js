b58b3dcb63681ad83f745a8a986c326d43d113b1
Renato <paulo.borupdebakker@dci-student.org>
2025-05-08 20:37:18 +0200

    translation unfineshed

---
+++ i18n/de.js
@@ -0,0 +1,76 @@
+export default {
+  translation: {
+    "nav": {
+      "destination": "Reiseziel",
+      "europe": "Europa",
+      "asia": "Asien",
+      "southAmerica": "Südamerika",
+      "activities": "Aktivitäten",
+      "travelTypes": "Reisearten"
+    },
+    "nav.europe": {
+      "france": "Frankreich",
+      "italy": "Italien",
+      "germany": "Deutschland",
+      "spain": "Spanien"
+    },
+    "nav.asia": {
+      "japan": "Japan",
+      "thailand": "Thailand",
+      "china": "China",
+      "southKorea": "Südkorea"
+    },
+    "nav.southAmerica": {
+      "brazil": "Brasilien",
+      "argentina": "Argentinien",
+      "peru": "Peru",
+      "colombia": "Kolumbien"
+    },
+    "nav.activities": {
+      "adventure": "Abenteuer",
+      "water": "Wassersport",
+      "cultural": "Kulturell",
+      "winter": "Wintersport"
+    },
+    "activities": {
+      "hiking": "Wandern",
+      "safari": "Safari",
+      "rafting": "Rafting",
+      "ziplining": "Ziplining",
+      "scuba": "Tauchen",
+      "surfing": "Surfen",
+      "snorkeling": "Schnorcheln",
+      "kayaking": "Kajakfahren",
+      "museums": "Museen",
+      "historicSites": "Historische Stätten",
+      "festivals": "Lokale Festivals",
+      "cooking": "Kochkurse",
+      "skiing": "Skifahren",
+      "snowboarding": "Snowboarden",
+      "iceClimbing": "Eisklettern"
+    },
+    "nav.travelTypes": {
+      "byGroup": "Nach Gruppe",
+      "byTheme": "Nach Thema",
+      "seasonal": "Saisonal"
+    },
+    "group": {
+      "solo": "Alleinreisende",
+      "couples": "Paare",
+      "family": "Familien",
+      "friends": "Freunde"
+    },
+    "theme": {
+      "wellness": "Wellness",
+      "food": "Essen & Wein",
+      "honeymoon": "Flitterwochen",
+      "volunteering": "Freiwilligenarbeit"
+    },
+    "season": {
+      "summer": "Sommerurlaub",
+      "winter": "Winterurlaub",
+      "spring": "Frühlingsferien",
+      "christmas": "Weihnachtsmärkte"
+    }
+  }
+};
---
+++ i18n/en.js
@@ -0,0 +1,76 @@
+export default {
+  translation: {
+    "nav": {
+      "destination": "Destination",
+      "europe": "Europe",
+      "asia": "Asia",
+      "southAmerica": "South America",
+      "activities": "Activities",
+      "travelTypes": "Travel Types"
+    },
+    "nav.europe": {
+      "france": "France",
+      "italy": "Italy",
+      "germany": "Germany",
+      "spain": "Spain"
+    },
+    "nav.asia": {
+      "japan": "Japan",
+      "thailand": "Thailand",
+      "china": "China",
+      "southKorea": "South Korea"
+    },
+    "nav.southAmerica": {
+      "brazil": "Brazil",
+      "argentina": "Argentina",
+      "peru": "Peru",
+      "colombia": "Colombia"
+    },
+    "nav.activities": {
+      "adventure": "Adventure",
+      "water": "Water Sports",
+      "cultural": "Cultural",
+      "winter": "Winter Sports"
+    },
+    "activities": {
+      "hiking": "Hiking",
+      "safari": "Safari",
+      "rafting": "Rafting",
+      "ziplining": "Ziplining",
+      "scuba": "Scuba Diving",
+      "surfing": "Surfing",
+      "snorkeling": "Snorkeling",
+      "kayaking": "Kayaking",
+      "museums": "Museums",
+      "historicSites": "Historic Sites",
+      "festivals": "Local Festivals",
+      "cooking": "Cooking Classes",
+      "skiing": "Skiing",
+      "snowboarding": "Snowboarding",
+      "iceClimbing": "Ice Climbing"
+    },
+    "nav.travelTypes": {
+      "byGroup": "By Group",
+      "byTheme": "By Theme",
+      "seasonal": "Seasonal"
+    },
+    "group": {
+      "solo": "Solo Travel",
+      "couples": "Couples",
+      "family": "Family",
+      "friends": "Friends"
+    },
+    "theme": {
+      "wellness": "Wellness",
+      "food": "Food & Wine",
+      "honeymoon": "Honeymoon",
+      "volunteering": "Volunteering"
+    },
+    "season": {
+      "summer": "Summer Holidays",
+      "winter": "Winter Getaways",
+      "spring": "Spring Break",
+      "christmas": "Christmas Markets"
+    }
+  }
+};
