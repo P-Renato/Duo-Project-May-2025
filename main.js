806276f907e2cd1aa6dfd01953fa77dabd1d8089
Renato <paulo.borupdebakker@dci-student.org>
2025-05-09 06:48:50 +0200

    translation updates

--- main.js
+++ main.js
@@ -1,11 +1,320 @@
-import en from "./i18n/en.js";
-import de from "./i18n/de.js";
 
-// Set up resources
-const resources = {
-  en,
-  de
-};
+  // const resources = {
+  //   en: {
+  //     translation: {
+  //       "nav": {
+  //     "destination": "Destination",
+  //     "europe": "Europe",
+  //     "asia": "Asia",
+  //     "southAmerica": "South America",
+  //     "activities": "Activities",
+  //     "travelTypes": "Travel Types"
+  //   },
+  //   "europe": {
+  //     "france": "France",
+  //     "italy": "Italy",
+  //     "germany": "Germany",
+  //     "spain": "Spain"
+  //   },
+  //   "nav.asia": {
+  //     "japan": "Japan",
+  //     "thailand": "Thailand",
+  //     "china": "China",
+  //     "southKorea": "South Korea"
+  //   },
+  //   "nav.southAmerica": {
+  //     "brazil": "Brazil",
+  //     "argentina": "Argentina",
+  //     "peru": "Peru",
+  //     "colombia": "Colombia"
+  //   },
+  //   "nav.activities": {
+  //     "adventure": "Adventure",
+  //     "water": "Water Sports",
+  //     "cultural": "Cultural",
+  //     "winter": "Winter Sports"
+  //   },
+  //   "activities": {
+  //     "hiking": "Hiking",
+  //     "safari": "Safari",
+  //     "rafting": "Rafting",
+  //     "ziplining": "Ziplining",
+  //     "scuba": "Scuba Diving",
+  //     "surfing": "Surfing",
+  //     "snorkeling": "Snorkeling",
+  //     "kayaking": "Kayaking",
+  //     "museums": "Museums",
+  //     "historicSites": "Historic Sites",
+  //     "festivals": "Local Festivals",
+  //     "cooking": "Cooking Classes",
+  //     "skiing": "Skiing",
+  //     "snowboarding": "Snowboarding",
+  //     "iceClimbing": "Ice Climbing"
+  //   },
+  //   "nav.travelTypes": {
+  //     "byGroup": "By Group",
+  //     "byTheme": "By Theme",
+  //     "seasonal": "Seasonal"
+  //   },
+  //   "group": {
+  //     "solo": "Solo Travel",
+  //     "couples": "Couples",
+  //     "family": "Family",
+  //     "friends": "Friends"
+  //   },
+  //   "theme": {
+  //     "wellness": "Wellness",
+  //     "food": "Food & Wine",
+  //     "honeymoon": "Honeymoon",
+  //     "volunteering": "Volunteering"
+  //   },
+  //   "season": {
+  //     "summer": "Summer Holidays",
+  //     "winter": "Winter Getaways",
+  //     "spring": "Spring Break",
+  //     "christmas": "Christmas Markets"
+  //   }
+  //     }
+  //   },
+  //   de: {
+  //     translation: {
+  //       "nav": {
+  //     "destination": "Reiseziel",
+  //     "europe": "Europa",
+  //     "asia": "Asien",
+  //     "southAmerica": "Südamerika",
+  //     "activities": "Aktivitäten",
+  //     "travelTypes": "Reisearten"
+  //   },
+  //   "nav.europe": {
+  //     "france": "Frankreich",
+  //     "italy": "Italien",
+  //     "germany": "Deutschland",
+  //     "spain": "Spanien"
+  //   },
+  //   "nav.asia": {
+  //     "japan": "Japan",
+  //     "thailand": "Thailand",
+  //     "china": "China",
+  //     "southKorea": "Südkorea"
+  //   },
+  //   "nav.southAmerica": {
+  //     "brazil": "Brasilien",
+  //     "argentina": "Argentinien",
+  //     "peru": "Peru",
+  //     "colombia": "Kolumbien"
+  //   },
+  //   "nav.activities": {
+  //     "adventure": "Abenteuer",
+  //     "water": "Wassersport",
+  //     "cultural": "Kulturell",
+  //     "winter": "Wintersport"
+  //   },
+  //   "activities": {
+  //     "hiking": "Wandern",
+  //     "safari": "Safari",
+  //     "rafting": "Rafting",
+  //     "ziplining": "Ziplining",
+  //     "scuba": "Tauchen",
+  //     "surfing": "Surfen",
+  //     "snorkeling": "Schnorcheln",
+  //     "kayaking": "Kajakfahren",
+  //     "museums": "Museen",
+  //     "historicSites": "Historische Stätten",
+  //     "festivals": "Lokale Festivals",
+  //     "cooking": "Kochkurse",
+  //     "skiing": "Skifahren",
+  //     "snowboarding": "Snowboarden",
+  //     "iceClimbing": "Eisklettern"
+  //   },
+  //   "nav.travelTypes": {
+  //     "byGroup": "Nach Gruppe",
+  //     "byTheme": "Nach Thema",
+  //     "seasonal": "Saisonal"
+  //   },
+  //   "group": {
+  //     "solo": "Alleinreisende",
+  //     "couples": "Paare",
+  //     "family": "Familien",
+  //     "friends": "Freunde"
+  //   },
+  //   "theme": {
+  //     "wellness": "Wellness",
+  //     "food": "Essen & Wein",
+  //     "honeymoon": "Flitterwochen",
+  //     "volunteering": "Freiwilligenarbeit"
+  //   },
+  //   "season": {
+  //     "summer": "Sommerurlaub",
+  //     "winter": "Winterurlaub",
+  //     "spring": "Frühlingsferien",
+  //     "christmas": "Weihnachtsmärkte"
+  //   }
+  //     }
+  //   }
+  // }
+  
+  const resources = {
+    en: {
+      translation: {
+        nav: {
+          destination: "Destination",
+          europe: "Europe",
+          asia: "Asia",
+          southAmerica: "South America",
+          activities: "Activities",
+          travelTypes: "Travel Types"
+        },
+        navItems: {
+          europe: {
+            france: "France",
+            italy: "Italy",
+            germany: "Germany",
+            spain: "Spain"
+          },
+          asia: {
+            japan: "Japan",
+            thailand: "Thailand",
+            china: "China",
+            southKorea: "South Korea"
+          },
+          southAmerica: {
+            brazil: "Brazil",
+            argentina: "Argentina",
+            peru: "Peru",
+            colombia: "Colombia"
+          },
+          activities: {
+            adventure: "Adventure",
+            water: "Water Sports",
+            cultural: "Cultural",
+            winter: "Winter Sports"
+          },
+          travelTypes: {
+            byGroup: "By Group",
+            byTheme: "By Theme",
+            seasonal: "Seasonal"
+          }
+        },
+        activities: {
+          hiking: "Hiking",
+          safari: "Safari",
+          rafting: "Rafting",
+          ziplining: "Ziplining",
+          scuba: "Scuba Diving",
+          surfing: "Surfing",
+          snorkeling: "Snorkeling",
+          kayaking: "Kayaking",
+          museums: "Museums",
+          historicSites: "Historic Sites",
+          festivals: "Local Festivals",
+          cooking: "Cooking Classes",
+          skiing: "Skiing",
+          snowboarding: "Snowboarding",
+          iceClimbing: "Ice Climbing"
+        },
+        group: {
+          solo: "Solo Travel",
+          couples: "Couples",
+          family: "Family",
+          friends: "Friends"
+        },
+        theme: {
+          wellness: "Wellness",
+          food: "Food & Wine",
+          honeymoon: "Honeymoon",
+          volunteering: "Volunteering"
+        },
+        season: {
+          summer: "Summer Holidays",
+          winter: "Winter Getaways",
+          spring: "Spring Break",
+          christmas: "Christmas Markets"
+        }
+      }
+    }
+    ,
+    de: {
+      "translation": {
+        "nav": {
+          "destination": "Reiseziel",
+          "europe": "Europa",
+          "asia": "Asien",
+          "southAmerica": "Südamerika",
+          "activities": "Aktivitäten",
+          "travelTypes": "Reisearten"
+        },
+        "navItems": {
+          "europe": {
+            "france": "Frankreich",
+            "italy": "Italien",
+            "germany": "Deutschland",
+            "spain": "Spanien"
+          },
+          "asia": {
+            "japan": "Japan",
+            "thailand": "Thailand",
+            "china": "China",
+            "southKorea": "Südkorea"
+          },
+          "southAmerica": {
+            "brazil": "Brasilien",
+            "argentina": "Argentinien",
+            "peru": "Peru",
+            "colombia": "Kolumbien"
+          },
+          "activities": {
+            "adventure": "Abenteuer",
+            "water": "Wassersport",
+            "cultural": "Kulturell",
+            "winter": "Wintersport"
+          },
+          "travelTypes": {
+            "byGroup": "Nach Gruppe",
+            "byTheme": "Nach Thema",
+            "seasonal": "Saisonal"
+          }
+        },
+        "activities": {
+          "hiking": "Wandern",
+          "safari": "Safari",
+          "rafting": "Rafting",
+          "ziplining": "Ziplining",
+          "scuba": "Tauchen",
+          "surfing": "Surfen",
+          "snorkeling": "Schnorcheln",
+          "kayaking": "Kajakfahren",
+          "museums": "Museen",
+          "historicSites": "Historische Stätten",
+          "festivals": "Lokale Festivals",
+          "cooking": "Kochkurse",
+          "skiing": "Skifahren",
+          "snowboarding": "Snowboarden",
+          "iceClimbing": "Eisklettern"
+        },
+        "group": {
+          "solo": "Alleinreisende",
+          "couples": "Paare",
+          "family": "Familien",
+          "friends": "Freunde"
+        },
+        "theme": {
+          "wellness": "Wellness",
+          "food": "Essen & Wein",
+          "honeymoon": "Flitterwochen",
+          "volunteering": "Freiwilligenarbeit"
+        },
+        "season": {
+          "summer": "Sommerurlaub",
+          "winter": "Winterurlaub",
+          "spring": "Frühlingsferien",
+          "christmas": "Weihnachtsmärkte"
+        }
+      }
+    }
+    
+  }
+
 
 // Initialize i18next
 i18next.init({
